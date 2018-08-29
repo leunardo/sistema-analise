1bdd97bb0919d3e81189981debc41405b0081ca4
Leonardo Alves <alexacaalves@gmail.com>
2018-08-29 20:49:32 -0300

    Add group and compare to salesman in itemService

--- SistemaAnalise/src/main/java/io/github/leunardo/sistemaanalise/service/ItemService.java
+++ SistemaAnalise/src/main/java/io/github/leunardo/sistemaanalise/service/ItemService.java
@@ -4,6 +4,10 @@ More in github.com/leunardo
 package io.github.leunardo.sistemaanalise.service;
 
 import io.github.leunardo.sistemaanalise.model.ItemModel;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  *
@@ -18,5 +22,26 @@ public class ItemService {
         double itemPrice = Double.parseDouble(values[2]);
         
         return new ItemModel(itemId, itemQuantity, itemPrice, salesmanName);
+    }
+    
+    public static Stream<Map.Entry<String, ArrayList<ItemModel>>> groupBySalesmanName (Stream<ItemModel> stream) {
+        Map<String, ArrayList<ItemModel>> map = new HashMap<>();
+        stream.forEach(item -> {
+            String salesman = item.getSalesmanName();
+            if (map.containsKey(salesman)) {
+                map.get(salesman).add(item);
+            } else {
+                map.put(salesman, new ArrayList<>());
+            }
+        });
+        
+        return map.entrySet().stream();
+    }
+    
+    public static Map.Entry<String, Double> compareSalesmanAliquota (Map.Entry<String, Double> a, Map.Entry<String, Double> b) {
+        if (a.getValue() > b.getValue()) {
+            return a;
+        } 
+        return b;
     }
 }

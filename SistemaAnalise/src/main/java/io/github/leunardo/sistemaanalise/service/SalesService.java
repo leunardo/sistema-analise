8ad5c4141330e09b67ed9ea2934b16895c062095
Leonardo Alves <alexacaalves@gmail.com>
2018-08-30 00:43:33 -0300

    fix comparation in sales service

--- SistemaAnalise/src/main/java/io/github/leunardo/sistemaanalise/service/SalesService.java
+++ SistemaAnalise/src/main/java/io/github/leunardo/sistemaanalise/service/SalesService.java
@@ -13,11 +13,10 @@ import io.github.leunardo.sistemaanalise.model.SalesModel;
 public class SalesService {
     
         
-    public static SalesModel compareSales(SalesModel a, SalesModel b) {
-        double priceA = a.getItems().stream().map(SalesService::getFullPriceOfItems).reduce(Double.NaN, Double::sum);
-        double priceB = b.getItems().stream().map(SalesService::getFullPriceOfItems).reduce(Double.NaN, Double::sum);
-    
-        return priceA > priceB ? a : b;
+    public static int compareSales(SalesModel a, SalesModel b) {
+        Double priceA = a.getItems().stream().map(SalesService::getFullPriceOfItems).reduce(Double.NaN, Double::sum);
+        Double priceB = b.getItems().stream().map(SalesService::getFullPriceOfItems).reduce(Double.NaN, Double::sum);
+        return priceA.intValue() - priceB.intValue();
     }
     
     private static double getFullPriceOfItems(ItemModel item) {
